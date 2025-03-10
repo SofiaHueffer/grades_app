be30a2dbfc0f13e7761b235c82f2a5ae15a22025
Sofia Hueffer <sofiahueffer@dyn3203-91.wlan.ic.ac.uk>
2025-03-10 19:24:31 +0000

    commit

--- app.py
+++ app.py
@@ -395,8 +395,9 @@ def min_score(year):
     flash("Target Set", "success")
 
     table_name = f"Year_{year}"
-
+    print('2 min score')
     cur.execute(f"""ALTER TABLE {table_name} ADD COLUMN IF NOT EXISTS min_score real;""")
+    print('3 min score')
     cur.execute(f"""
         UPDATE {table_name}
         SET min_score = ((%s - COALESCE(find_weight.Total_Score)) / last_weight)*100
@@ -412,10 +413,11 @@ def min_score(year):
         ) AS find_weight
         WHERE {table_name}.module = find_weight.module AND {table_name}.UserID = find_weight.UserID;
     """, (target_score,))
-
+    print('4 min score')
     conn.commit()
+    print('5 min score')
     return redirect(url_for('index'))
-
+    print('end min score')
 @app.route('/favicon.ico')
 def favicon():
     return '', 204
